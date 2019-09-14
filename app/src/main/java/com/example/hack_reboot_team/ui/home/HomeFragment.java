c52b49183b9442529afe5c0097fed65692065e31
Dmitriy <oiv2000@mail.ru>
2019-09-14 20:21:53 +0300

    Spisok

--- app/src/main/java/com/example/hack_reboot_team/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hack_reboot_team/ui/home/HomeFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hack_reboot_team.R;
@@ -24,6 +21,27 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        String user[] = {"Петя","Вася"};
+        item w1 = new item("Potato", 32,2, user);
+        item w2 = new item("Taxi", 86,1, user);
+
+        item arr[] = {w1,w2};
+
         return root;
+    }
+
+
+    private class item {
+        public String name;
+        public float price;
+        public int count;
+        public String[] users;
+
+        item(String name, float price, int count, String[] users){
+            this.name = name;
+            this.price = price;
+            this.count = count;
+            this.users = users;
+        }
     }
 }
